f4e85b9bfb2c11f0b81b047d0f9cb6e2a3f143ed
Richard Anemam <richardanemam@gmail.com>
2021-07-25 00:21:28 -0300

    create menu to order searches

--- app/src/main/java/com/example/codewarschallenge/presentation/activity/searchscreen/SearchScreenActivity.kt
+++ app/src/main/java/com/example/codewarschallenge/presentation/activity/searchscreen/SearchScreenActivity.kt
@@ -1,6 +1,9 @@
 package com.example.codewarschallenge.presentation.activity.searchscreen
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.codewarschallenge.R
 import com.example.codewarschallenge.databinding.ActivitySearchScreenBinding
@@ -26,6 +29,26 @@ class SearchScreenActivity: AppCompatActivity() {
         actionBar?.apply {
             setTitle(R.string.search_toolbar_title)
             setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_order, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.menu_order_by_rank -> {
+                //TODO order by rank
+                true
+            }
+            R.id.menu_order_by_search -> {
+                //TODO order by search
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
