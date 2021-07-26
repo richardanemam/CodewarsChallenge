f9c2b1f83ab98e2cc456be1d21854ee6a0dd66d5
Richard Anemam <richardanemam@gmail.com>
2021-07-26 20:46:12 -0300

    create model for authored challenges

---
+++ app/src/main/java/com/example/codewarschallenge/domain/model/AuthoredChallengesModel.kt
@@ -0,0 +1,21 @@
+package com.example.codewarschallenge.domain.model
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class AuthoredChallengesModel(
+    @SerializedName("data") val data: List<AuthoredChallengesData>?
+): Parcelable
+
+@Parcelize
+data class AuthoredChallengesData(
+    @SerializedName("id") val id: String?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("description") val description: String?,
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("rankName") val rankName: String?,
+    @SerializedName("tags") val tags: List<String>?,
+    @SerializedName("languages") val languages: List<String>?,
+): Parcelable
