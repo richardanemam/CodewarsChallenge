f4ecb22c8d7e4b30f546004d9fdbd209e5c62843
Richard Anemam <richardanemam@gmail.com>
2021-07-27 14:44:09 -0300

    create activity to display auth challenges

---
+++ app/src/main/java/com/example/codewarschallenge/presentation/activity/authoredchallengedetailsactivity/AuthoredChallengeDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.codewarschallenge.presentation.activity.authoredchallengedetailsactivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AuthoredChallengeDetailsActivity: AppCompatActivity() {
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+    }
+
+
+}
