efde1ae9325aadb719e55118f5fac663b5ac30e1
Richard Anemam <richardanemam@gmail.com>
2021-07-27 05:21:31 -0300

    add more languages

--- app/src/main/java/com/example/codewarschallenge/domain/helper/LanguagesMapHelper.kt
+++ app/src/main/java/com/example/codewarschallenge/domain/helper/LanguagesMapHelper.kt
@@ -10,8 +10,8 @@ data class MappedLanguages(
 object LanguagesMapHelper {
 
     fun getBestLanguage(languages: Languages): MappedLanguages? {
-        return mapLanguages(languages).maxByOrNull { score ->
-            score.score ?: 0
+        return mapLanguages(languages).maxByOrNull { language ->
+            language.score ?: 0
         }
     }
 
@@ -78,6 +78,301 @@ object LanguagesMapHelper {
             MappedLanguages(
                 languageName = "Cpp",
                 score = languages.cpp?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "R",
+                score = languages.r?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "C",
+                score = languages.c?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Cfml",
+                score = languages.cfml?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Clojure",
+                score = languages.clojure?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Cobol",
+                score = languages.cobol?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Commonlisp",
+                score = languages.commonlisp?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Coq",
+                score = languages.coq?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Crystal",
+                score = languages.crystal?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Dart",
+                score = languages.dart?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Elixir",
+                score = languages.elixir?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Elm",
+                score = languages.elm?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Erlang",
+                score = languages.erlang?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "F#",
+                score = languages.fSharp?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Factor",
+                score = languages.factor?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Forth",
+                score = languages.forth?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Fortran",
+                score = languages.fortran?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Groovy",
+                score = languages.groovy?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Haxe",
+                score = languages.haxe?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Julia",
+                score = languages.julia?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Kotlin",
+                score = languages.kotlin?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Lua",
+                score = languages.lua?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Nim",
+                score = languages.nim?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Objcy",
+                score = languages.objc?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Ocaml",
+                score = languages.ocaml?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Pascal",
+                score = languages.pascal?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Perl",
+                score = languages.perl?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Php",
+                score = languages.php?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Powershell",
+                score = languages.powershell?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Prolog",
+                score = languages.prolog?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Purescript",
+                score = languages.purescript?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Racket",
+                score = languages.racket?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Raku",
+                score = languages.raku?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Php",
+                score = languages.php?.score
+            )
+        )
+
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Reason",
+                score = languages.reason?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Rust",
+                score = languages.rust?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Scala",
+                score = languages.scala?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Shell",
+                score = languages.shell?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "VB",
+                score = languages.vb?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Solidity",
+                score = languages.solidity?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Swift",
+                score = languages.swift?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Sql",
+                score = languages.sql?.score
+            )
+        )
+
+        mappedLanguages.add(
+            MappedLanguages(
+                languageName = "Typescript",
+                score = languages.typeScript?.score
             )
         )
 
--- app/src/main/java/com/example/codewarschallenge/domain/model/UserModel.kt
+++ app/src/main/java/com/example/codewarschallenge/domain/model/UserModel.kt
@@ -14,13 +14,13 @@ data class UserModel(
     @SerializedName("skills") val skillsList: List<String>?,
     @SerializedName("ranks") val ranks: Ranks?,
     @SerializedName("codeChallenges") val codeChallenges: CodeChallenges?
-): Parcelable
+) : Parcelable
 
 @Parcelize
 data class Ranks(
     @SerializedName("overall") val overall: Overall?,
     @SerializedName("languages") val languages: Languages?
-): Parcelable
+) : Parcelable
 
 @Parcelize
 data class Overall(
@@ -28,7 +28,7 @@ data class Overall(
     @SerializedName("name") val name: String?,
     @SerializedName("color") val color: String?,
     @SerializedName("score") val score: Int?
-): Parcelable
+) : Parcelable
 
 @Parcelize
 data class Languages(
@@ -40,8 +40,49 @@ data class Languages(
     @SerializedName("java") val java: Java?,
     @SerializedName("go") val go: Go?,
     @SerializedName("haskell") val haskell: Haskell?,
-    @SerializedName("cpp") val cpp: Cpp?
-): Parcelable
+    @SerializedName("cpp") val cpp: Cpp?,
+    @SerializedName("elixir") val elixir: Elixir?,
+    @SerializedName("dart") val dart: Dart?,
+    @SerializedName("typescript") val typeScript: TypeScript?,
+    @SerializedName("fsharp") val fSharp: FSharp?,
+    @SerializedName("ocaml") val ocaml: Ocaml?,
+    @SerializedName("clojure") val clojure: Clojure?,
+    @SerializedName("rust") val rust: Rust?,
+    @SerializedName("c") val c: C?,
+    @SerializedName("php") val php: Php?,
+    @SerializedName("crystal") val crystal: Crystal?,
+    @SerializedName("swift") val swift: Swift?,
+    @SerializedName("shell") val shell: Shell?,
+    @SerializedName("lua") val lua: Lua?,
+    @SerializedName("sql") val sql: Sql?,
+    @SerializedName("r") val r: R?,
+    @SerializedName("objc") val objc: Go?,
+    @SerializedName("nim") val nim: Nim?,
+    @SerializedName("kotlin") val kotlin: Kotlin?,
+    @SerializedName("groovy") val groovy: Groovy?,
+    @SerializedName("scala") val scala: Scala?,
+    @SerializedName("solidity") val solidity: Solidity?,
+    @SerializedName("erlang") val erlang: Erlang?,
+    @SerializedName("fortran") val fortran: Fortran?,
+    @SerializedName("julia") val julia: Julia?,
+    @SerializedName("powershell") val powershell: PowerShel?,
+    @SerializedName("reason") val reason: Reason?,
+    @SerializedName("racket") val racket: Racket?,
+    @SerializedName("vb") val vb: Vb?,
+    @SerializedName("forth") val forth: Forth?,
+    @SerializedName("factor") val factor: Factor?,
+    @SerializedName("prolog") val prolog: Prolog?,
+    @SerializedName("cobol") val cobol: Cobol?,
+    @SerializedName("coq") val coq: Coq?,
+    @SerializedName("haxe") val haxe: Haxe?,
+    @SerializedName("elm") val elm: Elm?,
+    @SerializedName("cfml") val cfml: Cfml?,
+    @SerializedName("purescript") val purescript: PureScript?,
+    @SerializedName("commonlisp") val commonlisp: CommonLisp?,
+    @SerializedName("perl") val perl: Perl?,
+    @SerializedName("raku") val raku: Raku?,
+    @SerializedName("pascal") val pascal: Pascal?
+) : Parcelable
 
 @Parcelize
 data class JavaScript(
@@ -49,7 +90,7 @@ data class JavaScript(
     @SerializedName("name") val name: String?,
     @SerializedName("color") val color: String?,
     @SerializedName("score") val score: Int?
-): Parcelable
+) : Parcelable
 
 
 @Parcelize
@@ -58,7 +99,7 @@ data class Ruby(
     @SerializedName("name") val name: String?,
     @SerializedName("color") val color: String?,
     @SerializedName("score") val score: Int?
-): Parcelable
+) : Parcelable
 
 @Parcelize
 data class CoffeeScript(
@@ -66,7 +107,7 @@ data class CoffeeScript(
     @SerializedName("name") val name: String?,
     @SerializedName("color") val color: String?,
     @SerializedName("score") val score: Int?
-): Parcelable
+) : Parcelable
 
 @Parcelize
 data class CSharp(
@@ -74,7 +115,7 @@ data class CSharp(
     @SerializedName("name") val name: String?,
     @SerializedName("color") val color: String?,
     @SerializedName("score") val score: Int?
-): Parcelable
+) : Parcelable
 
 @Parcelize
 data class Python(
@@ -82,7 +123,7 @@ data class Python(
     @SerializedName("name") val name: String?,
     @SerializedName("color") val color: String?,
     @SerializedName("score") val score: Int?
-): Parcelable
+) : Parcelable
 
 @Parcelize
 data class Go(
@@ -90,7 +131,7 @@ data class Go(
     @SerializedName("name") val name: String?,
     @SerializedName("color") val color: String?,
     @SerializedName("score") val score: Int?
-): Parcelable
+) : Parcelable
 
 @Parcelize
 data class Java(
@@ -98,7 +139,7 @@ data class Java(
     @SerializedName("name") val name: String?,
     @SerializedName("color") val color: String?,
     @SerializedName("score") val score: Int?
-): Parcelable
+) : Parcelable
 
 @Parcelize
 data class Haskell(
@@ -106,7 +147,7 @@ data class Haskell(
     @SerializedName("name") val name: String?,
     @SerializedName("color") val color: String?,
     @SerializedName("score") val score: Int?
-): Parcelable
+) : Parcelable
 
 @Parcelize
 data class Cpp(
@@ -114,10 +155,342 @@ data class Cpp(
     @SerializedName("name") val name: String?,
     @SerializedName("color") val color: String?,
     @SerializedName("score") val score: Int?
-): Parcelable
+) : Parcelable
+
+@Parcelize
+data class Elixir(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+
+@Parcelize
+data class Dart(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class TypeScript(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class FSharp(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Ocaml(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Clojure(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Rust(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class C(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Php(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+
+@Parcelize
+data class Crystal(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Swift(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Shell(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Lua(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Sql(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class R(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Nim(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Objc(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Kotlin(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Groovy(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Scala(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Solidity(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Erlang(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Fortran(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Julia(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class PowerShel(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Reason(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Racket(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Vb(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Forth(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Factor(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Prolog(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Cobol(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Coq(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+
+@Parcelize
+data class Haxe(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Elm(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Cfml(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class PureScript(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class CommonLisp(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Perl(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+@Parcelize
+data class Raku(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
+
+
+@Parcelize
+data class Pascal(
+    @SerializedName("rank") val rank: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("color") val color: String?,
+    @SerializedName("score") val score: Int?
+) : Parcelable
 
 @Parcelize
 data class CodeChallenges(
     @SerializedName("totalAuthored") val totalAuthored: Int?,
     @SerializedName("totalCompleted") val totalCompleted: Int?
-): Parcelable
+) : Parcelable
