12a5252d5bf7060735b211d8ebf80ada8cb80b68
Richard Anemam <richardanemam@gmail.com>
2021-07-26 21:40:23 -0300

    create adapter to bind views for challenges info

---
+++ app/src/main/java/com/example/codewarschallenge/presentation/adapter/AuthoredChallengesAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.codewarschallenge.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.codewarschallenge.databinding.ItemChallengesBinding
+import com.example.codewarschallenge.domain.model.CompleteChallengesData
+
+class AuthoredChallengesAdapter(private val data: List<CompleteChallengesData>): RecyclerView.Adapter<AuthoredChallengesAdapter.AuthoredChallengesViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): AuthoredChallengesViewHolder {
+        val binding = ItemChallengesBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return AuthoredChallengesViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: AuthoredChallengesViewHolder, position: Int) {
+        holder.bindViews()
+    }
+
+    override fun getItemCount() = data.size
+
+    inner class AuthoredChallengesViewHolder(private val binding: ItemChallengesBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bindViews() {
+            binding.tvItemChallengesName.text = data[absoluteAdapterPosition].name
+            binding.tvItemChallengesDesciption.text = data[absoluteAdapterPosition].completedAt
+        }
+    }
+}
---
+++ app/src/main/java/com/example/codewarschallenge/presentation/adapter/CompleteChallengesAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.codewarschallenge.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.codewarschallenge.R
+import com.example.codewarschallenge.databinding.ItemChallengesBinding
+import com.example.codewarschallenge.domain.model.CompleteChallengesData
+
+class CompleteChallengesAdapter(private val data: List<CompleteChallengesData>): RecyclerView.Adapter<CompleteChallengesAdapter.CompleteChallengesViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CompleteChallengesViewHolder {
+        val binding = ItemChallengesBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CompleteChallengesViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CompleteChallengesViewHolder, position: Int) {
+       holder.bindViews()
+    }
+
+    override fun getItemCount() = data.size
+
+    inner class CompleteChallengesViewHolder(private val binding: ItemChallengesBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bindViews() {
+            binding.tvItemChallengesName.text = data[absoluteAdapterPosition].name
+            binding.tvItemChallengesDesciption.text = data[absoluteAdapterPosition].completedAt
+            binding.tvItemChallengesDescriptionTitle.text =
+                binding.tvItemChallengesDescriptionTitle
+                    .context
+                    .getString(R.string.challenges_item_completed_at)
+        }
+    }
+}
